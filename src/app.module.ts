ab9acddf9872b24aa471be7481e39d2240565ee1
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-07-26 18:57:29 +0300

    Created Chat Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
